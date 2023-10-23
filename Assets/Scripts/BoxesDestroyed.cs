964d217cd9a34485223f0b533a5657d06f4c1ae0
ItalianJackWEIRD <jackybox93@gmail.com>
2023-10-23 12:54:04 +0200

    Boxes Finished 2.0
    Boxes finished completely!!!

--- Assets/Scripts/BoxesDestroyed.cs
+++ Assets/Scripts/BoxesDestroyed.cs
@@ -4,10 +4,16 @@ using UnityEngine;
 
 public class destroyedBoxes : MonoBehaviour
 {
-    
+    private float moveSpeed = 4;
+
     void Start()
     {
         StartCoroutine(DestroyBox());
+    }
+
+    void Update()
+    {
+        gameObject.transform.position += new Vector3(moveSpeed * Time.deltaTime, 0, 0);  //tiles movment X direction axis
     }
 
     IEnumerator DestroyBox()
