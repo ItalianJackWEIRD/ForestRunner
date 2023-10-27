71b748aae239665437e879f9c3fb37664f2856d0
Samuele Capoccia <samusamu1701@gmail.com>
2023-10-27 18:57:52 +0200

    Jump almost finished
    Il salto funziona, il giocatore non trapassa il pavimento

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -13,37 +13,22 @@ public class Movement : MonoBehaviour
 
     private bool up = false;
 
-    /*public float jumpUpVelocity;
-    public float jumpDownVelocity;*/
-
     [SerializeField] float jumpHeight = 5;
     [SerializeField] float gravityScale = 5;
-    [SerializeField] Transform groundCheck;
-    [SerializeField] LayerMask ground;
+    [SerializeField] LayerMask groundMask;
 
     public float velocity;
 
-    //[SerializeField] float floorHeight = 0.0f;
-    //[SerializeField] Transform feet;
-    //[SerializeField] ContactFilter filter;
-    //public bool isGrounded;
-    //Collider[] results = new Collider[1];
-
-   /* public float jumpForce = 2f;
-
-    public float fallForce = 2.5f;
-
-    public Rigidbody rb;
-
-    private bool isGrounded;*/
+    [SerializeField] float floorHeight = 0.5f;
+    [SerializeField] Transform feet;
+    public bool isGrounded;
 
     public Material[] mat_sky;
 
     private void Start()
     {
         Player = GetComponent<Transform>();
-        //rb = GetComponent<Rigidbody>();
-        RenderSettings.skybox=mat_sky[UnityEngine.Random.Range(0 , 5)];//create random skybox 1-5
+        RenderSettings.skybox=mat_sky[UnityEngine.Random.Range(0 , 5)]; //create random skybox 1-5
     }
 
     private void Update()
@@ -94,58 +79,30 @@ public class Movement : MonoBehaviour
         }
         #endregion
 
-
-        /*if(Player.position.y == 0.0f)
-        {
-            velocity = 0;
-        }*/
         velocity += Physics.gravity.y * gravityScale * Time.deltaTime;
 
+        RaycastHit hit;
 
-        if(SwipeManager.swipeUp && IsGrounded())
+        if(Physics.Raycast(feet.position, Vector3.down, out hit, floorHeight, groundMask) && velocity < 0)
+        {
+            velocity = 0;
+            Vector3 surface = hit.point + Vector3.up * floorHeight;
+            transform.position = new Vector3(transform.position.x, surface.y, transform.position.z);
+            isGrounded = true;
+        }
+        else 
+        {
+            isGrounded = false;
+        }
+
+        if(SwipeManager.swipeUp && isGrounded)
         {
             velocity = Mathf.Sqrt(jumpHeight * -2 * (Physics.gravity.y * gravityScale));
         }
 
         transform.Translate(new Vector3(0, velocity, 0) * Time.deltaTime);
 
-       /* if(SwipeManager.swipeUp == true) {
-        if (rb.velocity.y > 0)
-        {
-            rb.velocity += Vector3.up * Physics.gravity.y * (jumpForce - 1) * Time.deltaTime;
-        }
-        else if(rb.velocity.y < 0)
-        {
-            rb.velocity += Vector3.up * Physics.gravity.y * (fallForce - 1) * Time.deltaTime;
-        }
-        }*/
-
-        /*  if (SwipeManager.swipeUp == true && Player.position.y <= 0f && up ==false)
-        {
-            up = true;
-        }
-
-        if(up == true && Player.position.y <= 1.5f)
-        {
-
-            Player.position += new Vector3(0, + jumpUpVelocity * Time.deltaTime  , 0);
-        }
-        else if(Player.position.y > 0f)
-        {
-           up = false;
-           Player.position += new Vector3(0, -jumpDownVelocity * Time.deltaTime  , 0);  
-        }
-        else if(Player.position.y < 0f)
-        {
-           Player.position += new Vector3(0, 0 , 0);  
-        }*/
-
         RenderSettings.skybox.SetFloat("_Rotation", Time.time * 5.0f); //rotate skybox
     
-    }
-
-    bool IsGrounded()
-    {
-        return Physics.CheckSphere(groundCheck.position, 0.1f, ground);
     }
 }
